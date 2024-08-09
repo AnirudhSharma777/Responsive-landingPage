e188b3d62729501204cdbcf10b70fab553c3a4f3
AnirudhSharmagithub01 <anirudhsharma857000@gmail.com>
2024-08-09 19:47:10 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Products from './components/Homepage/Products';
 import Home from './components/Homepage/Home';
 import Testimonials from './components/Homepage/Testimonials';
 import Features from './components/Homepage/Features';
-import { useRef } from 'react';
+import { useRef, useEffect, useState } from 'react';
+import { BsAirplaneEnginesFill } from "react-icons/bs";
 
 function App() {
 
@@ -15,30 +16,52 @@ function App() {
   const testimonials = useRef();
   const contact = useRef();
   const feature = useRef();
+  const scrollTop = useRef();
 
   const scrolHandler = (elmRef) => {
-    console.log(elmRef.current);
+    // console.log(elmRef.current);
     window.scrollTo({ top: elmRef.current.offsetTop, behavior: 'smooth' });
   };
 
+  const scrollToTop = (elmRef) => {
+    // console.log(elmRef.current);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    const handleScroll = (event) => {
+      if (window.scrollY > 300) {
+        setShow(true);
+      }
+      else {
+        setShow(false);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }, []);
   return (
-    <div className=" d-flex flex-column mx-auto">
-      <Container>       
+    <div className="position-relative d-flex flex-column mx-auto">
+      <Container>
         <Navbar expand="lg" className='d-flex justify-content-between'>
           <Navbar.Brand className='fs-5 fw-bold py-5 text-Capitilize' style={{ color: "#7e79e6" }}>Jillion Technologies</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          
-            <Navbar.Collapse id="basic-navbar-nav" >
-              <Nav className="ms-auto fw-semibold gap-4 ">
-                <Nav.Link>
-                  <a onClick={() => scrolHandler(home)} className='a' style={{ color: "#7e79e6" }}>Home</a>
-                </Nav.Link>
-                <Nav.Link onClick={() => scrolHandler(feature)} className='a' style={{ color: "#7e79e6" }}>Features</Nav.Link>
-                <Nav.Link onClick={() => scrolHandler(product)} className='a' style={{ color: "#7e79e6" }}>Products</Nav.Link>
-                <Nav.Link onClick={() => scrolHandler(testimonials)} className='a' style={{ color: "#7e79e6" }}>Testimonials</Nav.Link>
-                <Nav.Link onClick={() => scrolHandler(contact)} className='a' style={{ color: "#7e79e6" }}>Contact</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
+
+          <Navbar.Collapse id="basic-navbar-nav" >
+            <Nav className="ms-auto fw-semibold gap-4 ">
+              <Nav.Link>
+                <a onClick={() => scrolHandler(home)} className='a' style={{ color: "#7e79e6" }}>Home</a>
+              </Nav.Link>
+              <Nav.Link onClick={() => scrolHandler(feature)} className='a' style={{ color: "#7e79e6" }}>Features</Nav.Link>
+              <Nav.Link onClick={() => scrolHandler(product)} className='a' style={{ color: "#7e79e6" }}>Products</Nav.Link>
+              <Nav.Link onClick={() => scrolHandler(testimonials)} className='a' style={{ color: "#7e79e6" }}>Testimonials</Nav.Link>
+              <Nav.Link onClick={() => scrolHandler(contact)} className='a' style={{ color: "#7e79e6" }}>Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
       </Container>
 
@@ -46,7 +69,7 @@ function App() {
         <Home />
       </div>
       <div ref={feature}>
-        <Features/>
+        <Features />
       </div>
       <div ref={product}>
         <Products />
@@ -56,6 +79,9 @@ function App() {
       </div>
       <div ref={contact}>
         <Contact />
+      </div>
+      <div >
+        <BsAirplaneEnginesFill onClick={() => scrollToTop(scrollTop)} size={35} className={` text-secondary ${show === true ? 'd-block' : 'd-none'}`} style={{ position: 'fixed', right: 25, bottom: 50,backgroundColor:'transparent' }} />
       </div>
       <Footer />
     </div>
